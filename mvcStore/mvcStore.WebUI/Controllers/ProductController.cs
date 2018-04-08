87eeeb29758ce0309ae63784ea0d0afccf8ce95e
vitaliyNet <kmda.mail@gmail.com>
2018-04-08 18:39:01 +0300

    [VM Apr 8, 18] Adding ninject and mock list of products.

---
+++ mvcStore/mvcStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcStore.Domain.Abstract;
+
+namespace mvcStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+        
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
